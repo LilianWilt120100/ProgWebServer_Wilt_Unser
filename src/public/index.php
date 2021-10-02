f176464351d32b4368af8935eae192f5394beabe
Kévin <unserkevinpro@gmail.com>
2021-10-02 19:45:39 +0200

    Mise en place slim 3 eloquent

--- src/public/index.php
+++ src/public/index.php
@@ -1,1 +1,10 @@
 <?php
+
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->get('/', function(\Slim\Http\Request $request, \Slim\Http\Response $response) {
+    return $response->write('Salut Slim fonctionne');
+});
+$app->run();

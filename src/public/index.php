49303bb5d6eb74af1a6e7355af5e72a6fe029322
Kévin <unserkevinpro@gmail.com>
2021-10-04 11:06:42 +0200

    Ajout de Eloquent au projet

--- src/public/index.php
+++ src/public/index.php
@@ -5,6 +5,6 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 $app->get('/', function(\Slim\Http\Request $request, \Slim\Http\Response $response) {
-    return $response->write('Salut Slim fonctionne');
+    return $response->write('Salut Slim 3 fonctionne');
 });
 $app->run();

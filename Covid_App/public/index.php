19ad967ca52d8ae6ba503bf2e9e7cca06689522a
Kévin <unserkevinpro@gmail.com>
2021-10-04 18:15:40 +0200

    Migration sur Vs Code et Mise en place de Slim 4

--- Covid_App/public/index.php
+++ Covid_App/public/index.php
@@ -1,15 +1,17 @@
 <?php
+
+use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
-require '../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
+
+$app->get('/',function (Request $request, Response $responce, $parameters) {
+    $responce->getBody()->write('Hello World !');
+
+    return $responce;
 });
 
 $app->run();
